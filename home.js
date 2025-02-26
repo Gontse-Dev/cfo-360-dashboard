112cfa4fc14b826150b1405f4d21e6cbe7a8efe7
gontse-dev <gontses@adignite.co.za>
2025-02-26 12:16:31 +0200

    init

---
+++ home.js
@@ -0,0 +1,49 @@
+//Link redirects
+
+document.getElementById('knowledge-base').addEventListener('click', function() {
+    window.open('https://sites.google.com/cfo360.co.za/knowledgebase/main', '_blank');
+});
+
+document.getElementById('tickets').addEventListener('click', function() {
+    window.open('', '_blank');
+});
+
+document.getElementById('google-review').addEventListener('click', function() {
+    window.open('https://g.page/r/CUkTnai_47twEAE/review', '_blank');
+});
+
+document.getElementById('access-request-form').addEventListener('click', function() {
+    window.open('https://docs.google.com/forms/d/e/1FAIpQLSeBJMGwJftG4qbRw_F3my0tx5lcIl3wHr0uZtoKuF__rhXBbg/viewform', '_blank');
+});
+
+document.getElementById('billing-request').addEventListener('click', function() {
+    window.open('', '_blank');
+});
+
+document.getElementById('cancellation-request').addEventListener('click', function() {
+    window.open('', '_blank');
+});
+
+document.getElementById('client-cancellation-request-sa').addEventListener('click', function() {
+    window.open('https://cfo360.typeform.com/to/ulEqyEDN', '_blank');
+});
+
+document.getElementById('client-cancellation-request-uk').addEventListener('click', function() {
+    window.open('https://cfo360.typeform.com/to/Qowx81aE', '_blank');
+});
+
+document.getElementById('billing-changes-form').addEventListener('click', function() {
+    window.open('https://cfo360.typeform.com/to/kGt1mhK6', '_blank');
+});
+
+document.getElementById('sales-upsell-request').addEventListener('click', function() {
+    window.open('', '_blank');
+});
+
+document.getElementById('rss-feed').addEventListener('click', function() {
+    window.open('', '_blank');
+});
+
+document.getElementById('linkedin').addEventListener('click', function() {
+    window.open('https://www.linkedin.com/company/cfo360/', '_blank');
+});
